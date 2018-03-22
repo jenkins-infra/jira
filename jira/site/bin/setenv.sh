180c8a66ef77088a0c85aa1e9079df037ac170e8
olblak <me@olblak.com>
2018-03-22 10:24:06 +0100

    Enable garbage collection logging at startup

--- jira/site/bin/setenv.sh
+++ jira/site/bin/setenv.sh
@@ -6,3 +6,5 @@
 # tweak JVM heap settings to our liking. Remove what JIRA set, and insert our own
 export JAVA_OPTS="$(echo $JAVA_OPTS | sed -e 's/-Xms[^ ]*//' -e 's/-Xmx[^ ]*//' -e 's/-XX:MaxPermSize=[^ ]*//')"
 export JAVA_OPTS="-XX:MaxPermSize=256m -Xms1536m -Xmx3072m $JAVA_OPTS"
+# Version 7.4 Add Garbage collection logging on Jira Startup
+export JAVA_OPTS="-XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCCause -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=20M -Xloggc:\"%atlassian_logsdir%\atlassian-jira-gc-%%t.log $JAVA_OPTS\""
